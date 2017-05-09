e200f1c535b164c0179980a96cc78e6bf3daf164
saiba09 <saiba_sheikh@persistent.co.in>
2017-05-09 12:17:57 +0530

    Writing to text file

--- src/main/java/StreamingDataFlow/testProject/AllRides.java
+++ src/main/java/StreamingDataFlow/testProject/AllRides.java
@@ -3,6 +3,7 @@ import com.google.api.services.bigquery.model.TableRow;
 import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.coders.TableRowJsonCoder;
 import com.google.cloud.dataflow.sdk.io.PubsubIO;
+import com.google.cloud.dataflow.sdk.io.TextIO;
 import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
 import com.google.cloud.dataflow.sdk.runners.DataflowPipelineRunner;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
@@ -11,6 +12,8 @@ import com.google.cloud.dataflow.sdk.transforms.MapElements;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
 import com.google.cloud.dataflow.sdk.values.TypeDescriptor;
 import StreamingDataFlow.testProject.util.CustomPipelineOptions;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
@@ -32,18 +35,18 @@ public class AllRides {
   // "passenger_count":2
   // }
 
-  private static class PassThroughAllRides extends DoFn<TableRow, TableRow> {
+  private static class PassThroughAllRides extends DoFn<TableRow, String> {
     PassThroughAllRides() {}
 
     @Override
-    public void processElement(ProcessContext c) {
+    public void processElement(ProcessContext c) throws IOException {
       TableRow ride = c.element();
-
+      
       // Access to data fields:
       // float lat = Float.parseFloat(ride.get("latitude").toString());
       // float lon = Float.parseFloat(ride.get("longitude").toString());
 
-      c.output(ride);
+      c.output(ride.toPrettyString());
     }
   }
 
@@ -69,21 +72,22 @@ public class AllRides {
 
      // In Java 8 you can also use a simpler syntax through MapElements.
      // MapElements allows a single output element per input element.
-     .apply("pass all rides through 2",
-        MapElements.via((TableRow e) -> e).withOutputType(TypeDescriptor.of(TableRow.class)))
+//     .apply("pass all rides through 2",
+//        MapElements.via((TableRow e) -> e).withOutputType(TypeDescriptor.of(TableRow.class)))
 
      // In java 8, if you need to return zero one or more elements per input, you can use
      // the FlatMapElements syntax. It expects you to return an iterable and will
      // gather all of its values into the output PCollection.
-     .apply("pass all rides through 3",
-        FlatMapElements.via(
-          (TableRow e) -> {
-            List<TableRow> a = new ArrayList<>();
-              a.add(e);
-              return a;
-            }).withOutputType(TypeDescriptor.of(TableRow.class)))
+//     .apply("pass all rides through 3",
+//        FlatMapElements.via(
+//          (TableRow e) -> {
+//            List<TableRow> a = new ArrayList<>();
+//              a.add(e);
+//              return a;
+//            }).withOutputType(TypeDescriptor.of(TableRow.class)))
 
-     .apply(PubsubIO.Write.named("write to PubSub").topic(String.format("projects/%s/topics/%s", options.getSinkProject(), options.getSinkTopic())).withCoder(TableRowJsonCoder.of()));
+     .apply(TextIO.Write.named("write to PubSub").to("gs://synpuf-data/streamingFile.txt"));
+    		 //.topic(String.format("projects/%s/topics/%s", options.getSinkProject(), options.getSinkTopic())).withCoder(TableRowJsonCoder.of()));
     p.run();
   }
 }
